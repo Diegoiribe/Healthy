5ec75747932f40a6ea25da09290069b990b16a71
Diego Iribe <diegoiribe@192.168.68.108>
2025-08-13 11:43:47 -0700

    change x in dashboard

--- src/mobile/template/DashboardMobile.tsx
+++ src/mobile/template/DashboardMobile.tsx
@@ -87,7 +87,22 @@ export const DashboardMobile = ({
                 className="flex items-center justify-center w-10 h-10 text-xl font-bold text-white bg-red-400 rounded-full cursor-pointer hover:bg-red-700"
                 onClick={logOut}
               >
-                ↲
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  className="lucide lucide-log-out-icon lucide-log-out"
+                >
+                  <path d="m16 17 5-5-5-5" />
+                  <path d="M21 12H9" />
+                  <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+                </svg>
               </div>
             </div>
             <div
